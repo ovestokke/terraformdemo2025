31664578c241c0ff35a61c2b19bd1c4fc6cee801
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2025-01-30 12:58:11 +0100

    change

--- terraform/modules/storageaccount/main.tf
+++ terraform/modules/storageaccount/main.tf
@@ -4,7 +4,6 @@ resource "azurerm_storage_account" "this" {
   location                        = var.location
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
-  shared_access_key_enabled       = false
   default_to_oauth_authentication = true
   min_tls_version                 = "TLS1_2"
   public_network_access_enabled   = false
--- terraform/resources3-module.tf
+++ terraform/resources3-module.tf
@@ -1,6 +1,12 @@
 module "fagkveld_storageaccount" {
   source               = "./modules/storageaccount"
   storage_account_name = "capfagkveldstor1"
-  resource_group_name  = "capfagkveld-storage"
-  location             = "norwayeast"
+  resource_group_name  = azurerm_resource_group.storage_rg.name
+  location             = azurerm_resource_group.storage_rg.location
+}
+
+resource "azurerm_resource_group" "storage_rg" {
+  name     = "capfagkveld-storage"
+  location = "norwayeast"
+  
 }
